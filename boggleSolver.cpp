9977a17a636ee8ea1ab8fb232b33062a1c7cec31
Arjun S Nath <arjun.sn7c8@gmail.com>
2015-07-14 02:20:17 +0530

    Added classes for solving boggle

--- boggleSolver.cpp
+++ boggleSolver.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 class TrieNode
 {
+
 private:
     int value;
     vector<TrieNode*> children;
@@ -55,6 +56,7 @@ public:
 
 class Trie
 {
+
 private:
     int count;
     TrieNode* root;
@@ -107,16 +109,111 @@ public:
     }
 };
 
-int main(int argc, char const *argv[]) {
-    int n;
+class Boggle
+{
+
+private:
+    vector<string> boggleMatrix;
     Trie trie;
-    cin>>n;
-    while(n--)
+    int m, n;
+
+    void findWordsUtil(vector<vector<bool> >& visited, string& str, int row, int col,
+        vector<string>& result)
+    {
+        if(row < 0 || row >= m || col < 0 || col >= n)
+        {
+            return;
+        }
+
+        if(visited[row][col])
+        {
+            return;
+        }
+
+        visited[row][col] = true;
+        str = str + boggleMatrix[row][col];
+        if(trie.search(str))
+        {
+            result.push_back(str);
+        }
+
+        for(int i=row-1; i<=row+1; i++)
+        {
+            for(int j=col-1; j<=col+1; j++)
+            {
+                findWordsUtil(visited, str, i, j, result);
+            }
+        }
+
+        visited[row][col] = false;
+        str.erase(str.length()-1);
+    }
+
+public:
+    Boggle(vector<string> matrix, vector<string> dictionary)
+    {
+        boggleMatrix = matrix;
+        m = boggleMatrix.size();
+        n = boggleMatrix[0].size();
+        for(int i=0; i<dictionary.size(); i++)
+        {
+            trie.insert(dictionary[i]);
+        }
+    }
+
+    vector<string> findWords()
+    {
+        vector<vector<bool> > visited;
+        for(int i=0; i<m; i++)
+        {
+            vector<bool> temp;
+            for(int j=0; j<n; j++)
+            {
+                temp.push_back(false);
+            }
+            visited.push_back(temp);
+        }
+        string str = "";
+        vector<string> result;
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                findWordsUtil(visited, str, i, j, result);
+            }
+        }
+
+        return result;
+    }
+};
+
+int main(int argc, char const *argv[]) {
+    int c;
+    vector<string> dictionary;
+    cin>>c;
+    while(c--)
     {
         string temp;
         cin>>temp;
-        trie.insert(temp);
-        cout<<trie.search(temp)<<endl;
+        dictionary.push_back(temp);
+    }
+    int m;
+    cin>>m;
+    vector<string> boggleMatrix;
+    while(m--)
+    {
+        string temp;
+        cin>>temp;
+        boggleMatrix.push_back(temp);
+    }
+
+    Boggle boggle (boggleMatrix, dictionary);
+
+    vector<string> words = boggle.findWords();
+    cout<<endl;
+    for(int i=0; i<words.size(); i++)
+    {
+        cout<<words[i]<<endl;
     }
     return 0;
 }
